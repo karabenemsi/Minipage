f22efb83d9fe0bbbb3ba1598fcd9340853af05f6
karabenemsi <florian@florianlubitz.de>
2016-12-05 08:56:10 +0100

    Init

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//@prepros-append libaries/jquery-2.2.0.min.js
+//@prepros-append libaries/TweenMax.min.js
+//@prepros-append libaries/ScrollMagic.min.js
+//@prepros-append libaries/plugins/animation.gsap.min.js
+
+//Remove after developement
+//@prepros-append libaries/plugins/debug.addIndicators.min.js
